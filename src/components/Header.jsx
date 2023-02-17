16467ae69f8ec32347aca528a9a87478739c8a3a
naiiiden <na4ko359@gmail.com>
2023-02-17 08:53:32 +0200

    header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>Where in the world?</h1>
+            <button aria-label="Activate dark mode">
+                dark mode
+            </button>
+        </header>
+    )
+}
+
+export default Header;
