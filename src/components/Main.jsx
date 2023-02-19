e98cd6912e6d1ce380873bdb44d494627281e687
naiiiden <na4ko359@gmail.com>
2023-02-19 17:50:13 +0200

    add search/filter inputs

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,17 +13,30 @@ const Main = () => {
 
     return (
         <main>
-            {data.map((country) => (
-                <div className="country-container">
-                    <img src={country.flags["png"]} alt={country.flags["alt"]}/>
-                    <div className="text-container">
-                        <h2>{country.name.common}</h2>
-                        <p>Population: <span>{country.population}</span></p>
-                        <p>Region: <span>{country.region}</span></p>
-                        <p>Capital: <span>{country.capital}</span></p>
+            <div className="inputs-container">
+                <input type="text" name="" id="" aria-label="Search for a country by name"/>
+                <select name="" id="" aria-label="Filter countries by region">
+                    <option value="">Filter by region</option>
+                    <option value="">Africa</option>
+                    <option value="">America</option>
+                    <option value="">Asia</option>
+                    <option value="">Europe</option>
+                    <option value="">Oceania</option>
+                </select>
+            </div>
+            <div className="countries-container">
+                {data.map((country) => (
+                    <div className="country-container">
+                        <img src={country.flags["png"]} alt={country.flags["alt"]}/>
+                        <div className="text-container">
+                            <h2>{country.name.common}</h2>
+                            <p>Population: <span>{country.population}</span></p>
+                            <p>Region: <span>{country.region}</span></p>
+                            <p>Capital: <span>{country.capital}</span></p>
+                        </div>
                     </div>
-                </div>
-            ) )}
+                ) )}
+            </div>
         </main>
     )
 }
