8052c7294029c61be9746c5d2413dbac8245ae5e
naiiiden <na4ko359@gmail.com>
2023-02-20 17:24:04 +0200

    add missing search icon

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,11 +10,13 @@ const Main = () => {
         .catch((error) => console.log(error.message));
     }, []);
 
-
     return (
         <main>
             <div className="inputs-container">
                 <input type="text" name="" id="" aria-label="Search for a country by name"/>
+                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24">
+                    <path class="search-icon" d="M784 936 532 684q-30 24-69 38t-83 14q-109 0-184.5-75.5T120 476q0-109 75.5-184.5T380 216q109 0 184.5 75.5T640 476q0 44-14 83t-38 69l252 252-56 56ZM380 656q75 0 127.5-52.5T560 476q0-75-52.5-127.5T380 296q-75 0-127.5 52.5T200 476q0 75 52.5 127.5T380 656Z"/>
+                </svg>
                 <select name="" id="" aria-label="Filter countries by region">
                     <option value="">Filter by region</option>
                     <option value="">Africa</option>
