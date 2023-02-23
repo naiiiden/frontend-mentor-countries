7a60bf3b0abed6c82145385ffc7923217f1aa516
naiiiden <na4ko359@gmail.com>
2023-02-23 18:10:33 +0200

    text input add search functionality

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 
 const Main = () => {
-    const [data, setData] = useState([]);
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch("https://restcountries.com/v3.1/all")
@@ -17,11 +18,28 @@ const Main = () => {
         .catch((error) => console.log(error.message));
     }
 
+    function countriesByName(name) {
+        name === "" 
+        ? fetch(`https://restcountries.com/v3.1/all`) 
+        : fetch(`https://restcountries.com/v3.1/name/${name}`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Country not found");
+            }
+            return response.json()})
+        .then((data) => (console.log(data), setData(data)))
+        .catch((error) => {
+            console.log(error.message)
+            setData([])
+            setError("Country not found");
+        });
+    }
+
     return (
         <main>
             <div className="inputs-container">
                 <div className="text-input-wrapper">
-                    <input type="text" placeholder="Search for a country..." aria-label="Search for a country by name"/>
+                    <input type="text" placeholder="Search for a country..." aria-label="Search for a country by name" onChange={(e) => countriesByName(e.target.value)}/>
                     <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24">
                         <path className="search-icon" d="M784 936 532 684q-30 24-69 38t-83 14q-109 0-184.5-75.5T120 476q0-109 75.5-184.5T380 216q109 0 184.5 75.5T640 476q0 44-14 83t-38 69l252 252-56 56ZM380 656q75 0 127.5-52.5T560 476q0-75-52.5-127.5T380 296q-75 0-127.5 52.5T200 476q0 75 52.5 127.5T380 656Z"/>
                     </svg>
@@ -36,7 +54,8 @@ const Main = () => {
                 </select>
             </div>
             <div className="countries-container">
-                {data.map((country) => (
+                {/* {error && error !== "0" & <p>{error}</p>} */}
+                {data && data.length > 0 && data.map((country) => (
                     <div className="country-container" key={country.name.common}>
                         <img src={country.flags["png"]} alt={country.flags["alt"]}/>
                         <div className="text-container">
@@ -46,7 +65,8 @@ const Main = () => {
                             <p>Capital: <span>{country.capital}</span></p>
                         </div>
                     </div>
-                ) )}
+                ))}
+                {data && data.length === 0 && <p>{error}</p>}
             </div>
         </main>
     )
