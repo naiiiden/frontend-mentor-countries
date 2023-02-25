516b96a0ebf9df90e2732f170b1c115c7be3bab8
naiiiden <na4ko359@gmail.com>
2023-02-25 13:05:55 +0200

    style .details-container on mobile

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -19,17 +19,17 @@ const CountryDetails = () => {
   return (
     <main className="main-details">
         <img src={data.flags["svg"]} alt={data.flags["alt"]}/>
-        <div>
+        <div class="details-container">
           <h1>{data.name.common}</h1>
-          <div className="details-container">
-            <div className="details-subcontainer">
+          <div className="details-subcontainer">
+            <div className="details-text-wrapper">
               <p>Native name: <span>{data.name.nativeName?.[Object.keys(data.name.nativeName)[0]].common}</span></p>
               <p>Population: <span>{data.population}</span></p>
               <p>Region: <span>{data.region}</span></p>
               <p>Sub Region: <span>{data.subregion}</span></p>
               <p>Capital: <span>{data.capital}</span></p>
             </div>
-            <div className="details-subcontainer">
+            <div className="details-text-wrapper">
               <p>Top Level Domain: <span>{data.tld.join(", ")}</span></p>
               <p>Currencies: <span>{Object.values(data.currencies).map(currency => currency.name).join(", ")}</span></p>
               <p>Languages: <span>{Object.values(data.languages).join(", ")}</span></p>
